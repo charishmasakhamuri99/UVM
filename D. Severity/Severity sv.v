6060b30290408f064453fda9ff97f6e9d90f737d
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2025-12-26 20:20:46 +0530

    Severity sv

---
+++ D. Severity/Severity sv.v
@@ -0,0 +1,16 @@
+// severity
+//when you have given uvm_no_action then the simulator will execute the particular 
+//message but not diaplayed on the consle
+
+
+`include "uvm_macros.svh"//sv based used to invoke the uvm_macros
+ import uvm_pkg::*;//used to involve the classes
+ module tb;
+   initial begin
+     int verb;
+     uvm_top.set_report_severity_action(UVM_WARNING,UVM_NO_ACTION);
+     `uvm_info("Top_tb","Info meassge",UVM_NONE);
+     `uvm_error("Top_tb","ERROR MESSAGE");
+     `uvm_warning("Top_tb","warning message");
+   end
+ endmodule
